bd10a9c309facfd6cb22269dc7c60e245229be70
Christopher Rybicki <rybickic@amazon.com>
2021-07-15 12:19:57 -0700

    chore: project created with projen

---
+++ .projenrc.js
@@ -0,0 +1,17 @@
+const { ConstructLibraryCdk8s } = require('projen');
+const project = new ConstructLibraryCdk8s({
+  author: 'Christopher Rybicki',
+  authorAddress: 'rybickic@amazon.com',
+  cdk8sVersion: '1.0.0-beta.10',
+  defaultReleaseBranch: 'main',
+  name: 'cdk8s-grafana',
+  repositoryUrl: 'https://github.com/cdk8s-team/cdk8s-grafana.git',
+
+  // deps: [],                          /* Runtime dependencies of this module. */
+  // description: undefined,            /* The description is just a string that helps people understand the purpose of the package. */
+  // devDeps: [],                       /* Build dependencies for this module. */
+  // packageName: undefined,            /* The "name" in package.json. */
+  // projectType: ProjectType.UNKNOWN,  /* Which type of project this is (library/app). */
+  // release: undefined,                /* Add release management to this project. */
+});
+project.synth();

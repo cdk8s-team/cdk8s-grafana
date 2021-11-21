63857885abd23064e87208e2933c3026896c01e1
Eli Polonsky <epolon@amazon.com>
2021-11-21 19:59:51 +0200

    chore: add some keywords (#55)
    Signed-off-by: iliapolo <epolon@amazon.com>

--- .projenrc.js
+++ .projenrc.js
@@ -4,7 +4,13 @@ const project = new JsiiProject({
   description: 'Grafana construct for cdk8s.',
   author: 'Amazon Web Services',
   authorUrl: 'https://aws.amazon.com',
-  keywords: ['cdk', 'cdk8s'],
+  keywords: [
+    'kubernetes',
+    'grafana',
+    'cdk8s',
+    'dashboards',
+    'observability',
+  ],
 
   defaultReleaseBranch: 'main',
   repositoryUrl: 'https://github.com/cdk8s-team/cdk8s-grafana.git',

5b159c89b733d200703bb7b9f3c4273f71d750a3
iliapolo <epolon@amazon.com>
2023-02-17 22:24:52 +0200

    projen type
    Signed-off-by: iliapolo <epolon@amazon.com>

--- .projenrc.js
+++ .projenrc.js
@@ -1,13 +1,8 @@
-const { Cdk8sCommon } = require('@cdk8s/projen-common');
-const { cdk } = require('projen');
+const { Cdk8sTeamJsiiProject } = require('@cdk8s/projen-common');
 
-const project = new cdk.JsiiProject({
-  ...Cdk8sCommon.props,
-
+const project = new Cdk8sTeamJsiiProject({
   name: 'cdk8s-grafana',
   description: 'Grafana construct for cdk8s.',
-  author: 'Amazon Web Services',
-  authorUrl: 'https://aws.amazon.com',
   keywords: [
     'kubernetes',
     'grafana',
@@ -15,42 +10,15 @@ const project = new cdk.JsiiProject({
     'dashboards',
     'observability',
   ],
-
   defaultReleaseBranch: 'main',
-  repositoryUrl: 'https://github.com/cdk8s-team/cdk8s-grafana.git',
   projenUpgradeSecret: 'PROJEN_GITHUB_TOKEN',
   peerDeps: [
     'cdk8s',
     'constructs',
   ],
-
   devDeps: [
     '@cdk8s/projen-common',
   ],
-
-  publishToMaven: {
-    javaPackage: 'org.cdk8s.grafana',
-    mavenGroupId: 'org.cdk8s',
-    mavenArtifactId: 'cdk8s-grafana',
-  },
-
-  publishToPypi: {
-    distName: 'cdk8s-grafana',
-    module: 'cdk8s_grafana',
-  },
-
-  publishToNuget: {
-    dotNetNamespace: 'Org.Cdk8s.Grafana',
-    packageId: 'Org.Cdk8s.Grafana',
-  },
-
-  depsUpgradeOptions: {
-    workflowOptions: {
-      schedule: Cdk8sCommon.upgradeScheduleFor('cdk8s-grafana'),
-    },
-  },
 });
-
-new Cdk8sCommon(project);
 
 project.synth();

30bb3788d8a9a358aefe0ce0591ad494b7407616
iliapolo <epolon@amazon.com>
2023-02-20 19:43:53 +0200

    newer version
    Signed-off-by: iliapolo <epolon@amazon.com>

--- .projenrc.js
+++ .projenrc.js
@@ -1,5 +1,4 @@
 const { Cdk8sTeamJsiiProject } = require('@cdk8s/projen-common');
-const { javascript } = require('projen');
 
 const project = new Cdk8sTeamJsiiProject({
   name: 'cdk8s-grafana',
@@ -12,22 +11,13 @@ const project = new Cdk8sTeamJsiiProject({
     'observability',
   ],
   defaultReleaseBranch: 'main',
-  golang: false,
-
   peerDeps: [
     'cdk8s',
     'constructs',
   ],
-
   devDeps: [
     '@cdk8s/projen-common',
   ],
-
-  depsUpgradeOptions: {
-    workflowOptions: {
-      schedule: javascript.UpgradeDependenciesSchedule.expressions(['0 0 * * *']),
-    },
-  },
 });
 
 project.synth();
